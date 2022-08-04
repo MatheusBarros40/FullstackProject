fc10f3d3a522d4740b1ae5041c62c1f1e61e0951
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-08-04 11:44:06 -0300

    first commit

---
+++ fullstack-api/src/test/java/com/example/fullstack/api/FullstackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
