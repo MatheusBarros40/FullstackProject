8cb9109ffab61324c29d960f5d74537c5ac3f49d
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-08-05 00:01:07 -0300

    Front quase finalizado

--- backend-api/src/main/java/com/example/fullstack/api/repository/MovimentacaoRepository.java
+++ backend-api/src/main/java/com/example/fullstack/api/repository/MovimentacaoRepository.java
@@ -3,5 +3,8 @@ package com.example.fullstack.api.repository;
 import com.example.fullstack.api.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+    public List<Movimentacao> findByIdConta(Integer idConta);
 }
---
+++ frontend-app/src/app/components/movimentacao-list/movimentacao-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovimentacaoListComponent } from './movimentacao-list.component';
+
+describe('MovimentacaoListComponent', () => {
+  let component: MovimentacaoListComponent;
+  let fixture: ComponentFixture<MovimentacaoListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MovimentacaoListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MovimentacaoListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
